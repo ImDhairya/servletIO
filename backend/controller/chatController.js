332a0fa6169a3d807022fdfddb065c0b5ac19c0f
ImDhairya <98574278+ImDhairya@users.noreply.github.com>
2024-06-28 21:25:38 +0530

    adding previous chat db left

--- backend/controller/chatController.js
+++ backend/controller/chatController.js
@@ -48,3 +48,67 @@ export const addChat = async (req, res) => {
     });
   }
 };
+
+export const fetchChat = async (req, res) => {
+  try {
+    const {userId, receiver} = req.body;
+
+    if (!userId || !receiver) {
+      return;
+    }
+    const chat = await Chat.find({
+      $or: [
+        {sender: userId, receiver: receiver},
+        {sender: receiver, receiver: userId},
+      ],
+    });
+
+    if (!chat) {
+      return res.status(200).json({
+        message: "No chats found between these users",
+        success: true, // Indicate success even though no chats found
+        chats: [],
+      });
+    }
+
+    const senderChatId = await Users.findById({_id: userId}, "chats").exec();
+    
+    console.log(senderChatId.chats);
+    const receiverChatId = await Users.findById({_id: receiver}, "chats");
+
+    // console.log(senderChatId.chats);
+
+    // const chats = await Chat.find({_id: {$in: senderChatId.chats}}).exec();
+
+    // const chats = senderChatId.chats.filter((chatId) => {
+    //   return senderChatId.some((chat) => chat._id === chatId);
+    // });
+    // console.log(chats);
+
+    // console.log(chats, "senders chats");
+    // const senderChats = chat.find((element) => {
+    // console.log(element);
+    // senderChatId.chats.find((ch) => {
+    // console.log(ch, 'gibbrish');
+    // return ch._id === element._id;
+    // });
+    // if (element._id === )
+    // return element._id === sende;
+    // });
+    // if (!chat) {
+    //   return res.status("401").json({
+    //     message: "chats not found",
+    //     success: false,
+    //   });
+    // }
+    // return res.status(201).json({
+    //   chats: chat,
+    // });
+  } catch (error) {
+    console.error("Error fetching chat:", error);
+    return res.status(500).json({
+      message: "Internal Server Error",
+      success: false,
+    });
+  }
+};
--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 const router = express.Router();
 
-import {addChat} from "../controller/chatController.js";
+import {addChat, fetchChat} from "../controller/chatController.js";
 import isAuthenticated from "../authenticated/authenticated.js";
 
 router.route("/addChat").post(addChat);
+router.route("/fetchChat").post(fetchChat);
 
 export default router;
