28e2c243587563b4e8a18eb60d80a39301f01da7
ImDhairya <98574278+ImDhairya@users.noreply.github.com>
2024-07-01 21:42:37 +0530

    mongoose db change

--- backend/connect.js
+++ backend/connect.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 export const Connect = () => {
   try {
+    // enter your own mongoose database id
     mongoose
       .connect(
         "mongodb+srv://maheshbhatt181995:7kmDH42pr939TpCT@temp.zsulayp.mongodb.net/?retryWrites=true&w=majority&appName=temp"
